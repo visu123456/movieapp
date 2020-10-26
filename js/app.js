73cc1b0a2a63a617703fe5644d4ccd471f29b4a3
hiren-solanki1 <71432656+hiren-solanki1@users.noreply.github.com>
2020-10-26 12:22:18 +0530

    haaa

---
+++ js/app.js
@@ -0,0 +1,167 @@
+// var _0x1855=["\x61\x76\x65\x6E\x67\x65\x72\x73","\x6C\x6F\x67","\x23\x73\x65\x61\x72\x63\x68","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x23\x65\x78\x61\x6D\x70\x6C\x65\x49\x6E\x70\x75\x74\x45\x6D\x61\x69\x6C\x31","\x23\x6D\x6F\x76\x69\x65\x73\x2D\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x23\x6D\x6F\x76\x69\x65\x73\x2D\x73\x65\x61\x72\x63\x68\x61\x62\x6C\x65","\x64\x69\x76","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x63\x6C\x61\x73\x73","\x69\x6D\x61\x67\x65\x43\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x64\x61\x74\x61\x2D\x69\x64","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x69\x6D\x67\x20\x73\x72\x63\x3D\x22","\x22\x20\x61\x6C\x74\x3D\x22\x22\x20\x64\x61\x74\x61\x2D\x6D\x6F\x76\x69\x65\x2D\x69\x64\x3D\x22","\x22\x3E\x0D\x0A\x20\x20\x20\x20","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x76\x61\x6C\x75\x65","","\x45\x72\x72\x6F\x72\x3A\x20","\x6D\x65\x73\x73\x61\x67\x65","\x49\x6E\x74\x65\x72\x6E\x61\x6C\x20\x53\x65\x72\x76\x65\x72","\x6B\x65\x79","\x4E\x6F\x20\x6B\x65\x79\x20\x66\x6F\x75\x6E\x64\x21\x21\x21","\x69\x66\x72\x61\x6D\x65","\x73\x72\x63","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x79\x6F\x75\x74\x75\x62\x65\x2E\x63\x6F\x6D\x2F\x65\x6D\x62\x65\x64\x2F","\x77\x69\x64\x74\x68","\x68\x65\x69\x67\x68\x74","\x61\x6C\x6C\x6F\x77\x46\x75\x6C\x6C\x73\x63\x72\x65\x65\x6E","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x63\x6F\x6E\x74\x65\x6E\x74","\x3C\x70\x20\x69\x64\x3D\x22\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x63\x6C\x6F\x73\x65\x22\x3E\x58\x3C\x2F\x70\x3E","\x72\x65\x73\x75\x6C\x74\x73","\x6C\x65\x6E\x67\x74\x68","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x70\x20\x69\x64\x3D\x22\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x63\x6C\x6F\x73\x65\x22\x3E\x58\x3C\x2F\x70\x3E\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x70\x3E\x4E\x6F\x20\x54\x72\x61\x69\x6C\x65\x72\x20\x66\x6F\x75\x6E\x64\x20\x66\x6F\x72\x20\x74\x68\x69\x73\x20\x76\x69\x64\x65\x6F\x20\x69\x64\x20\x6F\x66\x20","\x69\x64","\x3C\x2F\x70\x3E\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20","\x68\x32","\x74\x69\x74\x6C\x65","\x66\x69\x72\x73\x74\x43\x68\x69\x6C\x64","\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65","\x73\x65\x63\x74\x69\x6F\x6E","\x6D\x6F\x76\x69\x65","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x64\x69\x76\x20\x63\x6C\x61\x73\x73\x3D\x22\x63\x6F\x6E\x74\x65\x6E\x74\x22\x3E\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x70\x20\x69\x64\x3D\x22\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x63\x6C\x6F\x73\x65\x22\x3E\x58\x3C\x2F\x70\x3E\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x3C\x2F\x64\x69\x76\x3E\x0D\x0A\x20\x20\x20\x20","\x6F\x6E\x63\x6C\x69\x63\x6B","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x45\x76\x65\x6E\x74\x3A\x20","\x74\x61\x72\x67\x65\x74","\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65","\x69\x6D\x67","\x6D\x6F\x76\x69\x65\x49\x64","\x64\x61\x74\x61\x73\x65\x74","\x70\x61\x72\x65\x6E\x74\x45\x6C\x65\x6D\x65\x6E\x74","\x6E\x65\x78\x74\x45\x6C\x65\x6D\x65\x6E\x74\x53\x69\x62\x6C\x69\x6E\x67","\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x64\x69\x73\x70\x6C\x61\x79","\x61\x64\x64","\x63\x6C\x61\x73\x73\x4C\x69\x73\x74","\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x63\x6C\x6F\x73\x65","\x72\x65\x6D\x6F\x76\x65"];const INITIAL_SEARCH_VALUE=_0x1855[0];const log=console[_0x1855[1]];const searchButton=document[_0x1855[3]](_0x1855[2]);;;const searchInput=document[_0x1855[3]](_0x1855[4]);const moviesContainer=document[_0x1855[3]](_0x1855[5]);const moviesSearchable=document[_0x1855[3]](_0x1855[6]);function createImageContainer(_0xc5ecx8,_0xc5ecx9){const _0xc5ecxa=document[_0x1855[8]](_0x1855[7]);_0xc5ecxa[_0x1855[11]](_0x1855[9],_0x1855[10]);_0xc5ecxa[_0x1855[11]](_0x1855[12],_0xc5ecx9);const _0xc5ecxb=`${_0x1855[13]}${_0xc5ecx8}${_0x1855[14]}${_0xc5ecx9}${_0x1855[15]}`;_0xc5ecxa[_0x1855[16]]= _0xc5ecxb;return _0xc5ecxa}function resetInput(){searchInput[_0x1855[17]]= _0x1855[18]}function handleGeneralError(_0xc5ecxe){log(_0x1855[19],_0xc5ecxe[_0x1855[20]]);alert(_0xc5ecxe[_0x1855[20]]|| _0x1855[21])}function createIframe(_0xc5ecx10){const _0xc5ecx11=(_0xc5ecx10&& _0xc5ecx10[_0x1855[22]])|| _0x1855[23];const _0xc5ecx12=document[_0x1855[8]](_0x1855[24]);_0xc5ecx12[_0x1855[25]]= `${_0x1855[26]}${_0xc5ecx11}${_0x1855[18]}`;_0xc5ecx12[_0x1855[27]]= 360;_0xc5ecx12[_0x1855[28]]= 315;_0xc5ecx12[_0x1855[29]]= true;return _0xc5ecx12}function insertIframeIntoContent(_0xc5ecx10,_0xc5ecx14){const _0xc5ecx15=document[_0x1855[8]](_0x1855[7]);const _0xc5ecx12=createIframe(_0xc5ecx10);_0xc5ecx15[_0x1855[30]](_0xc5ecx12);_0xc5ecx14[_0x1855[30]](_0xc5ecx15)}function createVideoTemplate(_0xc5ecx17){const _0xc5ecx14=this[_0x1855[31]];_0xc5ecx14[_0x1855[16]]= _0x1855[32];const _0xc5ecx18=_0xc5ecx17[_0x1855[33]]|| [];if(_0xc5ecx18[_0x1855[34]]=== 0){_0xc5ecx14[_0x1855[16]]= `${_0x1855[35]}${_0xc5ecx17[_0x1855[36]]}${_0x1855[37]}`;return};for(let _0xc5ecx19=0;_0xc5ecx19< 4;_0xc5ecx19++){const _0xc5ecx10=_0xc5ecx18[_0xc5ecx19];insertIframeIntoContent(_0xc5ecx10,_0xc5ecx14)}}function createSectionHeader(_0xc5ecx1b){const _0xc5ecx1c=document[_0x1855[8]](_0x1855[38]);_0xc5ecx1c[_0x1855[16]]= _0xc5ecx1b;return _0xc5ecx1c}function renderMovies(_0xc5ecx17){const _0xc5ecx1e=generateMoviesBlock(_0xc5ecx17);const _0xc5ecx1c=createSectionHeader(this[_0x1855[39]]);_0xc5ecx1e[_0x1855[41]](_0xc5ecx1c,_0xc5ecx1e[_0x1855[40]]);moviesContainer[_0x1855[30]](_0xc5ecx1e)}function renderSearchMovies(_0xc5ecx17){moviesSearchable[_0x1855[16]]= _0x1855[18];const _0xc5ecx1e=generateMoviesBlock(_0xc5ecx17);moviesSearchable[_0x1855[30]](_0xc5ecx1e)}function generateMoviesBlock(_0xc5ecx17){const _0xc5ecx21=_0xc5ecx17[_0x1855[33]];const _0xc5ecx22=document[_0x1855[8]](_0x1855[42]);_0xc5ecx22[_0x1855[11]](_0x1855[9],_0x1855[42]);for(let _0xc5ecx19=0;_0xc5ecx19< _0xc5ecx21[_0x1855[34]];_0xc5ecx19++){const {poster_path,id}=_0xc5ecx21[_0xc5ecx19];if(poster_path){const _0xc5ecx8=MOVIE_DB_IMAGE_ENDPOINT+ poster_path;const _0xc5ecx23=createImageContainer(_0xc5ecx8,id);_0xc5ecx22[_0x1855[30]](_0xc5ecx23)}};const _0xc5ecx24=createMovieContainer(_0xc5ecx22);return _0xc5ecx24}function createMovieContainer(_0xc5ecx22){const _0xc5ecxb=document[_0x1855[8]](_0x1855[7]);_0xc5ecxb[_0x1855[11]](_0x1855[9],_0x1855[43]);const _0xc5ecx26=`${_0x1855[44]}`;_0xc5ecxb[_0x1855[16]]= _0xc5ecx26;_0xc5ecxb[_0x1855[41]](_0xc5ecx22,_0xc5ecxb[_0x1855[40]]);return _0xc5ecxb}searchButton[_0x1855[45]]= function(_0xc5ecx27){_0xc5ecx27[_0x1855[46]]();const _0xc5ecx28=searchInput[_0x1855[17]];if(_0xc5ecx28){searchMovie(_0xc5ecx28)};resetInput()};document[_0x1855[45]]= function(_0xc5ecx27){log(_0x1855[47],_0xc5ecx27);const {tagName,id}=_0xc5ecx27[_0x1855[48]];if(tagName[_0x1855[49]]()=== _0x1855[50]){const _0xc5ecx29=_0xc5ecx27[_0x1855[48]][_0x1855[52]][_0x1855[51]];const _0xc5ecx22=_0xc5ecx27[_0x1855[48]][_0x1855[53]][_0x1855[53]];const _0xc5ecx14=_0xc5ecx22[_0x1855[54]];_0xc5ecx14[_0x1855[57]][_0x1855[56]](_0x1855[55]);getVideosByMovieId(_0xc5ecx29,_0xc5ecx14)};if(id=== _0x1855[58]){const _0xc5ecx14=_0xc5ecx27[_0x1855[48]][_0x1855[53]];_0xc5ecx14[_0x1855[57]][_0x1855[59]](_0x1855[55])}};searchMovie(INITIAL_SEARCH_VALUE);searchUpcomingMovies();getTopRatedMovies();searchPopularMovie();getTrendingMovies()
+const INITIAL_SEARCH_VALUE = 'avengers';
+const log = console.log;
+
+// Selecting elements from the DOM
+const searchButton = document.querySelector('#search');;
+const searchInput = document.querySelector('#exampleInputEmail1');
+const moviesContainer = document.querySelector('#movies-container');
+const moviesSearchable = document.querySelector('#movies-searchable');
+
+function createImageContainer(imageUrl, id) {
+    const tempDiv = document.createElement('div');
+    tempDiv.setAttribute('class', 'imageContainer');
+    tempDiv.setAttribute('data-id', id);
+
+    const movieElement = `
+        <img src="${imageUrl}" alt="" data-movie-id="${id}">
+    `;
+    tempDiv.innerHTML = movieElement;
+
+    return tempDiv;
+}
+
+function resetInput() {
+    searchInput.value = '';
+}
+
+function handleGeneralError(error) {
+    log('Error: ', error.message);
+    alert(error.message || 'Internal Server');
+}
+
+function createIframe(video) {
+    const videoKey = (video && video.key) || 'No key found!!!';
+    const iframe = document.createElement('iframe');
+    iframe.src = `http://www.youtube.com/embed/${videoKey}`;
+    iframe.width = 360;
+    iframe.height = 315;
+    iframe.allowFullscreen = true;
+    return iframe;
+}
+
+function insertIframeIntoContent(video, content) {
+    const videoContent = document.createElement('div');
+    const iframe = createIframe(video);
+
+    videoContent.appendChild(iframe);
+    content.appendChild(videoContent);
+}
+
+
+function createVideoTemplate(data) {
+    const content = this.content;
+    content.innerHTML = '<p id="content-close">X</p>';
+    
+    const videos = data.results || [];
+
+    if (videos.length === 0) {
+        content.innerHTML = `
+            <p id="content-close">X</p>
+            <p>No Trailer found for this video id of ${data.id}</p>
+        `;
+        return;
+    }
+
+    for (let i = 0; i < 4; i++) {
+        const video = videos[i];
+        insertIframeIntoContent(video, content);
+    }
+}
+
+function createSectionHeader(title) {
+    const header = document.createElement('h2');
+    header.innerHTML = title;
+
+    return header;
+}
+
+
+function renderMovies(data) {
+    const moviesBlock = generateMoviesBlock(data);
+    const header = createSectionHeader(this.title);
+    moviesBlock.insertBefore(header, moviesBlock.firstChild);
+    moviesContainer.appendChild(moviesBlock);
+}
+
+
+
+function renderSearchMovies(data) {
+    moviesSearchable.innerHTML = '';
+    const moviesBlock = generateMoviesBlock(data);
+    moviesSearchable.appendChild(moviesBlock);
+}
+
+function generateMoviesBlock(data) {
+    const movies = data.results;
+    const section = document.createElement('section');
+    section.setAttribute('class', 'section');
+
+    for (let i = 0; i < movies.length; i++) {
+        const { poster_path, id } = movies[i];
+
+        if (poster_path) {
+            const imageUrl = MOVIE_DB_IMAGE_ENDPOINT + poster_path;
+    
+            const imageContainer = createImageContainer(imageUrl, id);
+            section.appendChild(imageContainer);
+        }
+    }
+
+    const movieSectionAndContent = createMovieContainer(section);
+    return movieSectionAndContent;
+}
+
+
+
+// Inserting section before content element
+function createMovieContainer(section) {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const template = `
+        <div class="content">
+            <p id="content-close">X</p>
+        </div>
+    `;
+
+    movieElement.innerHTML = template;
+    movieElement.insertBefore(section, movieElement.firstChild);
+    return movieElement;
+}
+
+searchButton.onclick = function (event) {
+    event.preventDefault();
+    const value = searchInput.value
+
+   if (value) {
+    searchMovie(value);
+   }
+    resetInput();
+}
+
+// Click on any movies
+// Event Delegation
+document.onclick = function (event) {
+    log('Event: ', event);
+    const { tagName, id } = event.target;
+    if (tagName.toLowerCase() === 'img') {
+        const movieId = event.target.dataset.movieId;
+        const section = event.target.parentElement.parentElement;
+        const content = section.nextElementSibling;
+        content.classList.add('content-display');
+        getVideosByMovieId(movieId, content);
+    }
+
+    if (id === 'content-close') {
+        const content = event.target.parentElement;
+        content.classList.remove('content-display');
+    }
+}
+
+// Initialize the search
+searchMovie(INITIAL_SEARCH_VALUE);
+searchUpcomingMovies();
+getTopRatedMovies();
+searchPopularMovie();
+getTrendingMovies();
